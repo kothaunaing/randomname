7d9877d1eb38d219e7d11e38817677fd311eed43
kothaunaing <141340019+kothaunaing@users.noreply.github.com>
2023-11-19 00:10:04 +0630

    Add files via upload

---
+++ scripts/favourite-names.js
@@ -0,0 +1,100 @@
+import {displayHTML} from '../scripts/main.js';
+
+export let favouriteNames = JSON.parse(localStorage.getItem('favourite-names')) || [];
+
+export function addToFavourites() {
+  document.querySelectorAll('.add-to-fav-btn')
+    .forEach((favElement) => {
+      favElement.addEventListener('click', () => {
+        const { name } = favElement.dataset;
+        addOrRemove(favElement, name);
+        saveToStorage();
+        favouriteNamesHTML();
+      });
+    });
+}
+
+function saveToStorage() {
+  localStorage.setItem('favourite-names', JSON.stringify(favouriteNames));
+}
+
+function addOrRemove(favElement, name) {
+  let isFaved;
+
+  if (!favElement.classList.contains('added')) {
+    favElement.classList.add('added');
+    isFaved = 'added';
+    checkAndAdd(name);
+  }
+  else {
+    favElement.classList.remove('added');
+    isFaved = 'not-added';
+    removeNames(name);
+  }
+  favElement.innerHTML = `
+  <img class="faved-icon" src="images/fav-${isFaved}.png">
+  `;
+}
+
+function checkAndAdd(name) {
+  let repeat = false;
+
+  favouriteNames.forEach((favName) => {
+    if (name === favName) {
+      repeat = true;
+    }
+  });
+
+  if (!repeat) {
+    favouriteNames.push(name);
+  }
+}
+
+function removeNames(name) {
+  let newFavNames = [];
+
+  favouriteNames.forEach((favName) => {
+    if (name !== favName) {
+      newFavNames.push(favName);
+    }
+  });
+  favouriteNames = newFavNames;
+}
+
+export function favouriteNamesHTML() {
+  let html = '';
+
+  favouriteNames.forEach((name) => {
+    const nameLower = name.toLowerCase();
+    html += `
+      <div class="fav-name fav-name-${name}">
+        <div class="col1-name">${name}</div>
+        <button class="faved-btn" 
+        data-faved-name="${name}">
+          <img class="faved-icon" src="images/fav-added.png" />
+        </button>
+      </div>
+    `;
+  });
+
+  document.querySelector('.fav-names')
+    .innerHTML = html;
+  removeFavouriteNames();
+}
+
+function removeFavouriteNames() {
+  document.querySelectorAll('.faved-btn')
+    .forEach((buttonElement) => {
+      buttonElement.addEventListener('click', () => {
+        const { favedName } = buttonElement.dataset;
+        const nameElement = document.querySelector(`.fav-name-${favedName}`);
+        removeNames(favedName);
+        saveToStorage();
+        favouriteNamesHTML();
+        displayHTML();
+        ;
+      });
+    });
+}
+
+
